78d1b88ccf543efe14d24d8518256c5bbe6502c3
GhaaZaaal <aghazal88@gmail.com>
2024-03-21 04:26:12 +0200

    fixes in 8 & 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    inKey = ""
+    inKey = None
     inVal = 0
     for key, val in a_dictionary.items():
         if val > inVal:
