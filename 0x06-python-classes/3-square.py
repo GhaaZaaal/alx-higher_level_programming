388df75ce6fc3c575892139aad23c6aee712387f
GhaaZaaal <aghazal88@gmail.com>
2024-06-22 06:10:18 +0300

    New Edition 1

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,37 +3,31 @@
 
 
 class Square:
-    """Defines a square."""
+    """
+    This is a class named Square and have a size attribute
+    """
 
-    def __init__(self, size):
-        """Constructor.
+    def __init__(self, size=0):
+        """
+        Initializing The Attributes
 
         Args:
-            size: length of a side of the square
-        """
-        self.size = size
+            1- size => The Size of Any Side of The Square
 
-    @property
-    def size(self):
-        """Property for the length of a side of this square.
-        Raises:
-            TypeError: If size is not an integer
-            ValueError: If size is less than 0
+        Errors:
+            1- If size is not an integer
+            2- If size is less than zero
         """
-        return self.__size
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self._Square__size = size
 
     def area(self):
-        """Area of this square.
-
-        Returns:
-            The size squared
         """
-        return self.__size**2
+        Function to Calculate The Area of The Square Using Size attribute
+        """
+        return self._Square__size**2
