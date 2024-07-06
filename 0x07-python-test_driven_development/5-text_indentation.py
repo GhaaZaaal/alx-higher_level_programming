a64558bfe3fc12fcf0bdc3fc2383fd1b892b2cb5
GhaaZaaal <aghazal88@gmail.com>
2024-07-06 06:49:12 +0300

    New Edition 1

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+def text_indentation(text):
+    """Doc"""
+    if not isinstance(text, str) or text is None:
+        raise TypeError("text must be a string")
+    hola = True
+    for letter in text:
+        if letter in (".", "?", ":"):
+            print(letter, end="\n\n")
+            hola = False
+            continue
+        if hola is True:
+            print(letter, end="")
+        hola = True
+    print()
