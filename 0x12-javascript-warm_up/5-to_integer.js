39dc62a946c95e9d5794863ed8ed88f870f125ba
GhaaZaaal <aghazal88@gmail.com>
2024-10-07 01:26:03 +0300

    Train on If Condition - 2

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 
 if (Number(args[2])) {
-	console.log("My number:", Number(args[2]));
+  console.log('My number:', Number(args[2]));
 } else {
-	console.log("Not a number");
+  console.log('Not a number');
 }
