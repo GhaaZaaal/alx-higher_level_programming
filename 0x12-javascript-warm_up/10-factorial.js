d9aa7851742f128df11226e9433254c6039cc22a
GhaaZaaal <aghazal88@gmail.com>
2024-10-07 02:54:24 +0300

    Trainning on Recursion & Functions & Loop

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+function factorial(number) {
+  if (number < 0) {
+    return false;
+  } else if (number === 0) {
+    return true;
+  }
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(Number(args[2])));
